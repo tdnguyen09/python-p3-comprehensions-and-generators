1e30bbda0087999c64ba362ec11166e96233cfa2
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-04-26 23:07:27 +1000

    complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    evens_num_lists = [ n for n in num_list if n % 2 == 0]
+    return evens_num_lists
     pass
 
 def make_exclamation(sentence_list):
+    new_sentence_list = [word + '!' for word in sentence_list ]
+    return new_sentence_list
     pass
